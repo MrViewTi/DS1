16c7ab4151761b4d588d0f2f3f8b0178f158a77f
MrViewTi <34791114+MrViewTi@users.noreply.github.com>
2021-10-19 12:27:05 +0200

    Add files via upload

---
+++ SOAP/src/main/java/com/example/springsoap/MealRepository.java
@@ -0,0 +1,96 @@
+package com.example.springsoap;
+
+import javax.annotation.PostConstruct;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+
+import io.foodmenu.cs.webservice.*;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class MealRepository {
+    private static final Map<String, Meal> meals = new HashMap<String, Meal>();
+    private static final Map<String, Order> orders = new HashMap<String, Order>();
+
+    @PostConstruct
+    public void initData() {
+
+        Meal a = new Meal();
+        a.setName("Steak");
+        a.setDescription("Steak with fries");
+        a.setMealtype(Mealtype.MEAT);
+        a.setKcal(1100);
+        a.setPrice(5000);
+
+
+        meals.put(a.getName(), a);
+
+        Meal b = new Meal();
+        b.setName("Portobello");
+        b.setDescription("Portobello Mushroom Burger");
+        b.setMealtype(Mealtype.VEGAN);
+        b.setKcal(637);
+        b.setPrice(7000);
+
+
+        meals.put(b.getName(), b);
+
+        Meal c = new Meal();
+        c.setName("Fish and Chips");
+        c.setDescription("Fried fish with chips");
+        c.setMealtype(Mealtype.FISH);
+        c.setKcal(950);
+        c.setPrice(1);
+
+
+
+        meals.put(c.getName(), c);
+    }
+
+    public Meal findMeal(String name) {
+        Assert.notNull(name, "The meal's code must not be null");
+        return meals.get(name);
+    }
+
+    public Meal findBiggestMeal() {
+
+        if (meals == null) return null;
+        if (meals.size() == 0) return null;
+
+        var values = meals.values();
+        return values.stream().max(Comparator.comparing(Meal::getKcal)).orElseThrow(NoSuchElementException::new);
+
+    }
+
+    public Meal findCheapestMeal() {
+
+        if (meals == null) return null;
+        if (meals.size() == 0) return null;
+
+        var values = meals.values();
+        return values.stream().min(Comparator.comparing(Meal::getPrice)).orElseThrow(NoSuchElementException::new);
+
+    }
+    public Orderconfirmation addOrder(Order order) {
+        orders.put(order.getId(), order);
+        Meallist list = order.getMeals();
+        float price = 0;
+        for(String meal : list.getMeal()){
+            price += findMeal(meal).getPrice();
+        }
+        Orderconfirmation confirmation = new Orderconfirmation();
+        confirmation.setPrice(price);
+        confirmation.setTime("Now");
+        confirmation.setOrderid(order.getId());
+        return confirmation;
+
+    }
+
+
+}
